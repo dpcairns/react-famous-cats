3c4db112be1b9ec430ee34baa5b20e346f076071
Mack Berliner-Glasser <39198725+MackenzieBerliner-Glasser@users.noreply.github.com>
2019-06-04 17:27:39 -0700

    Added import for QUnit to exit

--- configs/test/tests.js
+++ configs/test/tests.js
@@ -1,3 +1,7 @@
 import './html-equal.js';
+import { app } from '../src/firebase.js'; // import may be different depending on file structure
 // // individual test files...
 // import './whatever.test.js';
+QUnit.done(() => {
+    app.delete();
+}); 
