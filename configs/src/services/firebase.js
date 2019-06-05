b99606bf1e9410be64cdd20d2750b9311e47ace4
Mack Berliner-Glasser <39198725+MackenzieBerliner-Glasser@users.noreply.github.com>
2019-06-04 17:33:08 -0700

    Update firebase.js

--- configs/src/services/firebase.js
+++ configs/src/services/firebase.js
@@ -2,7 +2,7 @@ const config = {
     // config goes here
 };
 
-firebase.initializeApp(config);
+export const app = firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const db = firebase.database();
