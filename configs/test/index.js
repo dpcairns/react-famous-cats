9910d2b562a0bf6c30ccb16a2e5007f38e44b1f0
martypdx <marty@martypdx.com>
2019-08-11 15:30:44 -0700

    update BC2 configs

--- configs/test/index.js
+++ configs/test/index.js
@@ -12,10 +12,7 @@ global.FormData = window.FormData;
 global.localStorage = window.localStorage;
 global.sessionStorage = window.sessionStorage;
 global.URLSearchParams = window.URLSearchParams;
-
-global.firebase = require('firebase/app');
-require('firebase/auth');
-require('firebase/database');
+global.URL = window.URL;
 
 require = require('esm')(module);
 module.exports = require('./tests.js');
