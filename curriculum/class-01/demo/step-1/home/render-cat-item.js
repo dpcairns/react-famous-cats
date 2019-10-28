dfe24d797b388aaab02fbd568c38c221addbf976
martypdx <marty@martypdx.com>
2019-10-27 19:35:56 -0700

    demo step-1: design template

---
+++ curriculum/class-01/demo/step-1/home/render-cat-item.js
@@ -0,0 +1,17 @@
+export default function renderCatItem() {
+    const html = /*html*/`
+        <li>
+            <div class="info-container">
+                <h2>Felix</h2>
+                <p class="cat-type">Tuxedo</p>
+            </div>
+
+            <div class="image-container">
+                <img src="assets/cats/felix.png" alt="Felix image">
+            </div>
+            <p class="year">1892</p>
+        </li>
+    `;
+
+    return html;
+}
---
+++ curriculum/class-01/demo/step-1/test/render-cat-item.test.js
@@ -0,0 +1,27 @@
+import renderCatItem from '../home/render-cat-item.js';
+const test = QUnit.test;
+
+QUnit.module('Render Cat Item');
+
+test('renders html from data', assert => {
+    // arrange
+    const expected = /*html*/`
+        <li>
+            <div class="info-container">
+                <h2>Felix</h2>
+                <p class="cat-type">Tuxedo</p>
+            </div>
+
+            <div class="image-container">
+                <img src="assets/cats/felix.png" alt="Felix image">
+            </div>
+            <p class="year">1892</p>
+        </li>
+    `;
+
+    // act
+    const html = renderCatItem();
+    
+    // assert
+    assert.htmlEqual(html, expected);
+});
