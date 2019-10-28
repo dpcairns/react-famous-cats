eabe72ef9cc193371fb85cceea8fd1a2b1d9acc6
martypdx <marty@martypdx.com>
2019-10-27 19:45:11 -0700

    step 2: dynamic data rendering

---
+++ curriculum/class-01/demo/step-2/home/app.js
@@ -0,0 +1,21 @@
+// import cat data
+import cats from '../data/cats.js';
+// import html-to-DOM utility
+import htmlToDOM from '../util/html-to-DOM.js';
+// import renderCatItem function
+import renderCatItem from './render-cat-item.js';
+
+// reference the list
+const list = document.querySelector('.cats');
+
+// loop the cat data
+cats.forEach(cat => {
+    // call template function to get html
+    const html = renderCatItem(cat);
+
+    // make DOM from html
+    const dom = htmlToDOM(html);
+
+    // append to the list
+    list.appendChild(dom);
+});
