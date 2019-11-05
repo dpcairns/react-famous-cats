624e559e8080edc55f96f30df3a806ca7b800010
Danny Cairns <dpcairns@gmail.com>
2019-11-04 17:12:07 -0800

    initial commit

--- configs/src/Component.js
+++ configs/src/Component.js
@@ -14,11 +14,11 @@ class Component {
 
     renderDOM() {
         const html = this.renderHTML();
-        if (typeof(html) !== 'string') {
+        if (typeof (html) !== 'string') {
             throw new Error(`Component "${this.constructor.name}" needs to return an html string from renderHTML`);
         }
 
-        const dom = htmlToDOM(html);
+        const dom = htmlToDOM(html, this.constructor.name);
 
         // remember the root Element for later for replacing or removing
         this.rootElement = dom;
@@ -37,9 +37,9 @@ class Component {
         props = props || {};
         // update the props:
         Object.assign(this.props, props);
-        
+
         const oldRoot = this.rootElement;
-        
+
         if (!oldRoot) {
             throw new Error(`"update()" was called on Component "${this.constructor.name}", but no prior render has happened. Be sure to call ".renderDOM()" before using ".update()"`);
         }
--- configs/src/html-to-DOM.js
+++ configs/src/html-to-DOM.js
@@ -1,12 +1,12 @@
-function htmlToDOM(html) {
+function htmlToDOM(html, componentName) {
     const template = document.createElement('template');
     template.innerHTML = html;
     const content = template.content;
 
     if (content.children.length > 1) {
-        throw new Error('html needs to have single parent element');
+        throw new Error(`html in ${componentName} needs to have single parent element`);
     }
-    
+
     const firstElementChild = content.firstElementChild;
     return firstElementChild;
 }
