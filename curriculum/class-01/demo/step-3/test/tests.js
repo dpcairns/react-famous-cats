4e98b1b73448c2d6932f23dd139a3b8c314d634f
Danny Cairns <dpcairns@gmail.com>
2020-02-10 16:30:37 -0800

    delete

--- curriculum/class-01/demo/step-3/test/tests.js
+++
@@ -1,7 +0,0 @@
-// import utilities and custom assertions
-import './html-equal.js';
-// import util tests (you don't need to do this)
-import './html-equal.test.js';
-import './html-to-dom.test.js';
-// import tests
-import './render-cat-item.test.js';
